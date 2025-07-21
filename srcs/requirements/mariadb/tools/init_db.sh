d4a6e1dc48cdeca847794cd6dd5275d157c03def
mohamed chamsa <moha.chamsa@gmail.com>
2025-07-21 22:55:07 +0200

    docker-compose -f srcs/docker-compose.yml down -v

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 set -e
 
-# Read secrets
+# Read secrets as root (they are only readable by root)
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 DB_PASSWORD=$(cat /run/secrets/db_password)
+
+# Export them so they can be used by mysql user
+export DB_ROOT_PASSWORD
+export DB_PASSWORD
+
+# Ensure proper ownership of mysql directories
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /var/run/mysqld
 
 # Initialize database if not exists
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Initializing database..."
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    su-exec mysql mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
-    # Start MariaDB temporarily
-    mysqld_safe --user=mysql --datadir=/var/lib/mysql --socket=/var/run/mysqld/mysqld.sock &
+    # Start MariaDB temporarily as mysql user
+    su-exec mysql mysqld_safe --user=mysql --datadir=/var/lib/mysql --socket=/var/run/mysqld/mysqld.sock &
     sleep 5
 
     # Wait for MariaDB to start
-    while ! mysqladmin ping --silent; do
+    while ! su-exec mysql mysqladmin ping --silent; do
         sleep 1
     done
 
     # Set root password and create database
-    mysql -u root <<-EOSQL
+    su-exec mysql mysql -u root <<-EOSQL
         UPDATE mysql.user SET Password=PASSWORD('${DB_ROOT_PASSWORD}') WHERE User='root';
         DELETE FROM mysql.user WHERE User='';
         DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
@@ -33,8 +41,8 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 EOSQL
 
     # Stop temporary MariaDB
-    mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
+    su-exec mysql mysqladmin -u root -p"${DB_ROOT_PASSWORD}" shutdown
 fi
 
-# Start MariaDB normally
-exec mysqld_safe --user=mysql --datadir=/var/lib/mysql
+# Start MariaDB normally as mysql user
+exec su-exec mysql mysqld_safe --user=mysql --datadir=/var/lib/mysql
